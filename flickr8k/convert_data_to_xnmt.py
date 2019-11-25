150ed943217b2d820183aaf62c657e1bc2ee09b5
Mark A Hasegawa-Johnson <jhasegaw@illinois.edu>
2019-11-25 15:28:49 -0600

    multi-ref BLEU is now working.  Festvox is not yet working.

--- flickr8k/convert_data_to_xnmt.py
+++ flickr8k/convert_data_to_xnmt.py
@@ -19,7 +19,7 @@ print('Loaded {} image vectors'.format(len(all_images)))
 
 ###################################################################################
 # Read the transcripts
-phone_types = set()
+vocab = set()
 splits = ['train','test','val']
 transcripts = { split:{} for split in splits }
 no_image = set()
@@ -34,7 +34,9 @@ for split in splits:
                     try:
                         with open('labels/align_%s_%s.txt'%(imagename,cindex)) as ali_file:
                             phones = [ line.split()[0] for line in ali_file.readlines() ]
-                            transcripts[split][imagename].append(phones)
+                            if len(phones)>0:
+                                transcripts[split][imagename].append(phones)
+                                vocab |= set(phones)
                     except:
                         no_transcript.add('labels/align_%s_%s.txt'%(imagename,cindex))
                 if len(transcripts[split][imagename])==0:   # 
@@ -51,7 +53,7 @@ with open('flickr40k_phones_train.txt','w') as textfile:
     with open('flickr40k_tagged_train.txt','w') as tagfile:
         with h5py.File('flickr40k_cnnfeats_train.h5', "w") as h5file:
             for imagename in sorted(transcripts['train'].keys()):
-                for phones in transcripts['train'][imagename]:
+                for (cindex,phones) in enumerate(transcripts['train'][imagename]):
                     textfile.write(' '.join(phones)+"\n")
                     tagfile.write('%d %s_%s '%(num_written,imagename,cindex)+' '.join(phones)+'\n')
                     h5file.create_dataset('%d'%(num_written), data=all_images[imagename])
@@ -70,4 +72,10 @@ for split in ['val','test']:
                     phones=transcripts[split][imagename][cindex]
                     textfile.write(' '.join(phones)+"\n")
                     tagfile.write('%d %s_%s '%(nwritten,imagename,cindex)+' '.join(phones)+'\n')
+
+###################################################################################
+# Write vocab.txt
+with open('vocab.txt','w') as f:
+    for phone in sorted(vocab):
+        f.write(phone+'\n')
 
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -i
 #####################################################################
 # image2speech
 #
@@ -19,7 +19,7 @@
 # out, after you have done them once.
 #
 #####################################################################
-steps="09"
+steps="11 12"
 
 #####################################################################
 # Step 01
@@ -71,7 +71,7 @@ if [ -n "`echo $steps | grep 04`" ]; then
     echo ""
     echo "04: Run the VGG16 model"
     cd flickr8k
-    source activate vgg16_flickr8k_cnnfeats_env
+    conda activate vgg16_flickr8k_cnnfeats_env
     python vgg16_flickr8k_cnnfeats.py
     cd ..
 fi
@@ -129,7 +129,7 @@ if [ -n "`echo $steps | grep 08`" ]; then
     git clone https://github.com/neulab/xnmt
     cd xnmt
     conda create -n xnmt python=3.6
-    source activate xnmt
+    conda activate xnmt
     pip install -r requirements.txt
     python setup.py install
     conda deactivate
@@ -143,17 +143,123 @@ if [ -n "`echo $steps | grep 09`" ]; then
     echo ""
     echo "09: Train the CNNFEATS-to-phones translation model using XNMT on force-aligned phones"
     cd flickr8k
-    source activate xnmt
-    python ../xnmt/xnmt/xnmt_run_experiments.py --settings=debug flickr8k_im2ph.yaml
+    conda activate xnmt
+    # 128d attender, with and w/o decoder norm
+    python ../xnmt/xnmt/xnmt_run_experiments.py --settings=debug im2ph128att.yaml
+    python ../xnmt/xnmt/xnmt_run_experiments.py --settings=debug im2ph128att_0norm.yaml
+    # 64d attender, with and w/o decoder norm
+    python ../xnmt/xnmt/xnmt_run_experiments.py --settings=debug im2ph64att.yaml
+    python ../xnmt/xnmt/xnmt_run_experiments.py --settings=debug im2ph64att_0norm.yaml
     conda deactivate
     cd ..
 fi
 
-# Step 10: 
-# Step 11: download festival and festvox
-# Step 12: train the synthesis model from force-aligned audio captions?
-# Step 13: Testing: run XNMT to convert test-corpus CNNFEATS to phones
-# Step 14: Testing: run festvox to generate output speech from phones
+#################################################################################
+# # Step 10: Download festival and festvox
+if [ -n "`echo $steps | grep 10`" ]; then
+    echo "#######################################################################"
+    echo ""
+    echo "10: Download festival and festvox"
+    
+    # Try to make CMU speech tools
+    wget http://festvox.org/packed/festival/2.5/speech_tools-2.5.0-release.tar.gz
+    tar xzvf speech_tools-2.5.0-release.tar.gz
+    cd speech_tools
+    ./configure
+    gnumake
+    echo export PATH='$PATH':`pwd`/bin >> ~/.bashrc
+    echo export ESTDIR=`pwd` >> ~/.bashrc
+    echo export LD_LIBRARY_PATH='$LD_LIBRARY_PATH':`pwd`/lib >> ~/.bashrc
+    echo export INCLUDE_PATH='$INCLUDE_PATH':`pwd`/include >> ~/.bashrc
+    . ~/.bashrc
+    cd ..
+    
+    # Try to make festival
+    wget http://festvox.org/packed/festival/2.5/festival-2.5.0-release.tar.gz
+    tar xzvf festival-2.5.0-release.tar.gz
+    wget http://festvox.org/packed/festival/2.5/festlex_CMU.tar.gz
+    tar xzvf festlex_CMU.tar.gz
+    cd festival
+    gnumake
+    cd ..
+    
+    # Try to make festvox
+    wget http://festvox.org/festvox-2.7/festvox-2.7.0-release.tar.gz
+    tar xzvf festvox-2.7.0-release.tar.gz
+    cd festvox
+    ./configure
+    make
+    echo export FESTVOXDIR=`pwd` >> ~/.bashrc
+    ~/.bashrc
+    cd ..
+fi
+
+###############################################################################
+# Step 11: create the clustergen working directory, and get a detailed listing of its contents
+if [ -n "`echo $steps | grep 11`" ]; then
+    source ~/.bashrc  # Get the necessary path variables
+    echo "#######################################################################"
+    echo ""
+    echo "11: Create the clustergen working directory; copy wavs and phone transcriptions."
+
+    mkdir flickr8k_cg
+    cd flickr8k_cg
+    $FESTVOXDIR/src/clustergen/setup_cg image2speech L1phones flickr8k_cg
+
+    if [ -d wav ]; then rmdir wav; fi
+    cp -r ../flickr8k/flickr_audio/wavs wav
+    awk '{printf("( %s \"",$2);for(i=3;i<NF;i++){printf("%s ",$i)};printf("%s\" )\n",$NF);}' ../flickr8k/flickr40k_tagged_train.txt > etc/txt.done.data
+
+    ############################################33
+    # TODO: at this point, generate from
+    # vocab.txt (the phones) a dictionary like festival/lib/dicts/cmu/cmulex.scm.
+    # Create code like festival/lib/lexicons.scm::setup_cmu_lex to load it,
+    # insert that in the preamble of
+    # flickr8k/flickr8k_cg/festvox/image2speech_L1phones_flickr8k_cg_lexicon.scm,
+    # then change it to (lex.select "flickr8k")
+fi
+
+###############################################################################
+# Step 12: use do_build to generate wrong prompts -- just trying to see what's required
+if [ -n "`echo $steps | grep 12`" ]; then
+    source ~/.bashrc  # Get the necessary path variables
+    echo "#######################################################################"
+    echo ""
+    echo "12: Run the do_build scripts, to generate prompt files for clustergen."
+    echo "  Generates the files flickr8k_cg/prompt.lab/* and flickr8k_cg/prompt.utt/*."
+    echo "  Please check those files, to make sure they look reasonable."
+    cd flickr8k_cg
+
+    ./bin/do_build build_prompts
+    ./bin/do_build label
+    ./bin/do_build build_utts    
+fi
+
+###############################################################################
+# Step 13: clustergen
+if [ -n "`echo $steps | grep 13`" ]; then
+    source ~/.bashrc  # Get the necessary path variables
+    echo "#######################################################################"
+    echo ""
+    echo "13: Run clustergen to generate the synthetic voice image2speech_L1phones_flickr8k"
+
+    cd flickr8k_cg
+
+    ./bin/do_clustergen f0
+    ./bin/do_clustergen mcep
+    ./bin/do_clustergen voicing
+    ./bin/do_clustergen combine_coeffs_v
+
+    ./bin/do_clustergen generate_statenames
+    ./bin/do_clustergen cluster
+    ./bin/do_clustergen dur
+fi
+    
+# Step 14: Testing: run festival(?) to generate output speech from the files
+# flickr8k/flickr40k_phones_val_hyp*.txt and flickr8k/flickr40k_phones_test_hyp*.txt
+# using the voice image2speech_L1phones_flickr8k.
+
+
 
 
 
